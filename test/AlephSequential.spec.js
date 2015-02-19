0d7af0f19947643265bddebe3670b624c04c7d43
Pasi Tuominen <pasi.e.tuominen@helsinki.fi>
2015-02-19 12:27:21 +0200

    first commit

---
+++ test/AlephSequential.spec.js
@@ -0,0 +1,46 @@
+/*jshint mocha:true*/
+"use strict";
+
+var chai = require('chai');
+var expect = chai.expect;
+var Serializers = require('../lib/index.js');
+var fs = require('fs');
+var path = require('path');
+
+describe('AlephSequential', function() {
+
+	var filesPath = path.resolve(__dirname, "files/AlephSequential");
+	var testFiles = fs.readdirSync(filesPath)
+					.filter(function(fileName) { return fileName.indexOf("from") == 0});
+
+	testFiles.forEach(function(fromFile) {
+		var toFile = fromFile.replace("from", "to");
+		console.log(typeof filesPath, typeof toFile);
+		it('should convert file "' + fromFile + '" to file "' + toFile + '".', function(done) {
+
+			var fromFilePath = path.resolve(filesPath, fromFile);
+			var toFilePath = path.resolve(filesPath, toFile);
+
+			var expectedRecord = fs.readFileSync(toFilePath, 'utf8');
+			var reader = new Serializers.AlephSequentialReader(fs.createReadStream(fromFilePath));
+
+			var parsedRecords = [];
+			reader.on('data', function(record) {
+				parsedRecords.push(record);
+			});
+
+			reader.on('end', function() {
+
+				expect(parsedRecords).to.have.length(1);
+
+				var parsedRecordAsString = parsedRecords[0].toString();
+				expect(parsedRecordAsString).to.equal(expectedRecord);
+				
+				done();
+			});
+
+
+
+		});
+	});
+});
